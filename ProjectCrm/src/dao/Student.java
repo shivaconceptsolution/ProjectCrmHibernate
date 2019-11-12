f5ffa6851e878fe4d1c80e2802daf1c5de341dae
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2019-11-11 21:38:03 -0800

    Add files via upload

---
+++ ProjectCrm/src/dao/Student.java
@@ -0,0 +1,34 @@
+package dao;
+
+public class Student {
+
+	private int rno;
+	private String sname;
+	private String branch;
+	private int fees;
+	public int getRno() {
+		return rno;
+	}
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public int getFees() {
+		return fees;
+	}
+	public void setFees(int fees) {
+		this.fees = fees;
+	}
+	
+}
---
+++ ProjectCrm/src/emp/Employee.java
@@ -0,0 +1,34 @@
+package emp;
+
+public class Employee {
+private String username;
+private String password;
+private String email;
+private String designation;
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getDesignation() {
+	return designation;
+}
+public void setDesignation(String designation) {
+	this.designation = designation;
+}
+
+
+}
